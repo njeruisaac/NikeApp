2d9c2e84113994c4b5be470a9327e19d81f86f5f
Isaac Njeru <njeruisaac.in@gmail.com>
2023-12-12 20:13:32 +0300

    add cartlist

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -1,9 +1,27 @@
-import {Text} from "react-native";
+import {FlatList, StyleSheet, Text, View} from "react-native";
+import cart from "../data/cart";
+import CartListItem from "../components/CartListItem";
 
 const ShoppingCart = () =>{
   return (
-      <Text>Shopping Cart</Text>
+      <FlatList
+          data={cart}
+          renderItem={({item})=> <CartListItem cartItem={item}/> }
+          ListFooterComponent={()=>(
+              <View style={styles.totalsContainer}>
+                <View style={styles.row}>
+                    <Text> SubTotal</Text>
+                    <Text> $410.00</Text>
+                </View>
+              </View>
+          )}
+      />
   )
 };
+const styles= StyleSheet.create({
+    totalsContainer:{
+
+    },
+});
 
 export default ShoppingCart;
